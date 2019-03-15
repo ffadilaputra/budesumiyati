96c07674f3a8fa81714c19986b7ea0150d52cf1f
ffadilaputra <i.fadilaputra@gmail.com>
2019-03-15 08:02:05 +0700

    Codeship test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const uniqueRandomArray = require('unique-random-array')
 const quotesBude = require('./quote/data.json')
 
+
 exports.all = quotesBude
 exports.random = uniqueRandomArray(quotesBude)
 
